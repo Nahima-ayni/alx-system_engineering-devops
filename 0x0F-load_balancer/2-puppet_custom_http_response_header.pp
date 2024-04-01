ae431bc9bc291fe307a1ec8b00f605ac3e0be71e
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-01 21:10:44 +0300

    Load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#Add a custom HTTP header with Puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/conf.d/custom_headers.conf':
+  ensure  => present,
+  content => "server_tokens off;\nmore_set_headers \"X-Served-By: ${::fqdn}\";",
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/conf.d/custom_headers.conf'],
+}
