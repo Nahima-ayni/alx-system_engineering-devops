1a7e85d65c71d488316eeebab3e8e92f8a1e3938
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-22 16:02:40 +0300

    RESTful API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,15 +7,20 @@ import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    user_res = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    todos_res = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+    user_res = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    todos_res = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
     user = user_res.json()
     todos = todos_res.json()
 
     tasks = []
     for todo in todos:
-        task = {"task": todo.get('title'), "completed": todo.get('completed'), "username": user.get('username')}
+        task = {
+            "task": todo.get('title'),
+            "completed": todo.get('completed'),
+            "username": user.get('username')}
         tasks.append(task)
 
     with open(f'{employee_id}.json', 'w') as json_file:
