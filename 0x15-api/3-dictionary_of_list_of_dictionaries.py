6ad61eb9ccc080e917cc39e835bf584ab830da4c
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-22 16:48:24 +0300

    RESTful API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""extend your Python script to export data in the JSON format."""
 
 import json
 import requests
 import sys
 
 
-def todo_list_progress():
+if __name__ == "__main__":
     users_res = requests.get('https://jsonplaceholder.typicode.com/users')
     todos_res = requests.get('https://jsonplaceholder.typicode.com/todos')
 
