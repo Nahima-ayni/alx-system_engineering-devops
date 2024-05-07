3e66203224199c2df98c19c45febf1a2c8d8fb18
Nahima-ayni <nahimamadey1@gmail.com>
2024-05-07 22:47:27 +0300

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """0. How many subs?"""
-
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    url = f'https://www.reddit.com/r/<subreddit>/about.json'.format(subreddit)
-
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
